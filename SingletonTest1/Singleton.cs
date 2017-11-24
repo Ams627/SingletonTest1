9f4c74950ffac6a5614483b87916d15116cfe5bd
Adrian Sims <asims@parkeon.com>
2017-11-24 11:57:56 +0000

    Add project files.

---
+++ SingletonTest1/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media;
+
+namespace SingletonTest1
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        public bool IsMcardAvailable { get; set; } = true;
+        public Brush Colour { get; set; } = Brushes.AliceBlue;
+
+        public string MyString { get; set; } = "Hello, World";
+        public static Singleton Instance => GetInstance();
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        private Singleton()
+        {
+            System.Diagnostics.Debug.WriteLine("Create a singleton!");
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,18 @@
+rem echo off
+echo done >c:\temp\gitdone.txt
+git init SingletonTest1
+cd SingletonTest1
+git add SingletonTest1/App.xaml
+git add SingletonTest1/App.xaml.cs
+git add SingletonTest1/App.config
+git add SingletonTest1/MainWindow.xaml
+git add SingletonTest1/MainWindow.xaml.cs
+git add SingletonTest1/Properties/AssemblyInfo.cs
+git add SingletonTest1/app.config
+git add SingletonTest1/Properties/Settings.settings
+git add SingletonTest1/Properties/Resources.Designer.cs
+git add SingletonTest1/Properties/Resources.resx
+git add SingletonTest1/Properties/Settings.Designer.cs
+git add SingletonTest1/SingletonTest1.csproj
+git add SingletonTest1.sln
+git add .gitignore
